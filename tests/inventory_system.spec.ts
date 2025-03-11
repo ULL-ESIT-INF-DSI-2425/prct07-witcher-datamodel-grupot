b875df721d3b7805d22d36c2f8d386ab90055b30
Alejandro M. Linares <alu0101476476@ull.edu.es>
2025-03-11 17:53:40 +0000

    minor adjust

--- tests/inventory_system.spec.ts
+++ tests/inventory_system.spec.ts
@@ -7,7 +7,6 @@ import { Good } from "../src/goods";
 import { Customer } from "../src/other_clients";
 //import exp from "constants";
 
-let white_wolf_inn = new InventorySystem();
 const customer1 = new Customer(1, "Geralt", "Brujo", "Rivia");
 const customer2 = new Customer(2, "Doni", "Enano", "Novigrado");
 const good1 = new Good(1, "Pocion de Golondrina", "Pocion de Brujo", "Murcielago", 0.1, 100);
@@ -16,20 +15,8 @@ const merchant1 = new Merchant(1, "Segredus de Continente", "General", "Velen");
 const merchant2 = new Merchant(2, "Iker de Rivia", "Herrero", "Rivia");
 
 describe("listItems", () => {
-  // test("Prueba básica de añadir y borrar customers", () => {
-  //   white_wolf_inn.addCustomer(customer1);
-  //   expect(white_wolf_inn.customers.length).toBe(1);
-  //   expect(white_wolf_inn.customers[1]).toBe(customer1);
-  // });
-  // test("Prueba básica de añadir y borrar bienes", () => {
-  //   white_wolf_inn.addItem(good1);
-  //   white_wolf_inn.addItem(good2);
-  //   expect(white_wolf_inn.items.length).toBe(2);
-  //   expect(white_wolf_inn.items[2]).toBe(good2);
-  //   white_wolf_inn.removeItem(2);
-  //   expect(white_wolf_inn.items[2]).toBe(undefined);
-  // });
   test("Funcionamiento normal", () => {
+    let white_wolf_inn = new InventorySystem();
     white_wolf_inn.addItem(good1);
     white_wolf_inn.addItem(good2);
     expect(white_wolf_inn.listItems("name", true)).toStrictEqual([good2, good1]);
