da7fdd46b3d1b1c1eb61b54bd7a69afa8cfa0d9d
Alejandro M. Linares <alu0101476476@ull.edu.es>
2025-03-11 17:39:28 +0000

    test removeCustomer

--- tests/inventory_system.spec.ts
+++ tests/inventory_system.spec.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-expressions */
-import { describe, expect, expectTypeOf, it, test, vi} from "vitest";
+import { describe, expect, expectTypeOf, it, test, vi, afterEach} from "vitest";
 import { InventorySystem } from '../src/inventory_system';
 import { Merchant } from "../src/mercants";
 import { Good } from "../src/goods";
@@ -15,7 +15,7 @@ const good2 = new Good(2, "Colonia de Lirio y Grosellas", "Una colonia muy espec
 const merchant1 = new Merchant(1, "Segredus de Continente", "General", "Velen");
 const merchant2 = new Merchant(2, "Iker de Rivia", "Herrero", "Rivia");
 
-describe("Pruebas de Inventario", () => {
+describe("listItems", () => {
   // test("Prueba básica de añadir y borrar customers", () => {
   //   white_wolf_inn.addCustomer(customer1);
   //   expect(white_wolf_inn.customers.length).toBe(1);
@@ -29,7 +29,7 @@ describe("Pruebas de Inventario", () => {
   //   white_wolf_inn.removeItem(2);
   //   expect(white_wolf_inn.items[2]).toBe(undefined);
   // });
-  test("Prueba de list items", () => {
+  test("Funcionamiento normal", () => {
     white_wolf_inn.addItem(good1);
     white_wolf_inn.addItem(good2);
     expect(white_wolf_inn.listItems("name", true)).toStrictEqual([good2, good1]);
@@ -37,6 +37,10 @@ describe("Pruebas de Inventario", () => {
 });
 
 describe("addItem", () => {
+  afterEach(() => {
+    vi.restoreAllMocks(); 
+  });
+
   test("Funcionamiento normal", () => {
     let my_system = new InventorySystem();
     const spy = vi.spyOn(my_system, "addItem");
@@ -49,6 +53,10 @@ describe("addItem", () => {
 });
 
 describe("removeItem", () => {
+  afterEach(() => {
+    vi.restoreAllMocks(); 
+  });
+
   test("Funcionamiento normal", () => {
     let my_system = new InventorySystem();
     my_system.addItem(good2);
@@ -57,5 +65,33 @@ describe("removeItem", () => {
     my_system.removeItem(2);
     expect(spy).toHaveBeenCalledTimes(1);
     expect(my_system.items).toHaveLength(0);
+  });
+});
+
+describe("Manipulación de customers", () => {
+  afterEach(() => {
+    vi.restoreAllMocks(); 
+  });
+
+  let my_system = new InventorySystem();
+  test("Adicion de clientes", () => {
+    const spy = vi.spyOn(my_system, "addCustomer");
+    my_system.addCustomer(customer1);
+    expect(my_system.customers).toHaveLength(1);
+    expect(spy).toHaveBeenCalledTimes(1);
+    my_system.addCustomer(customer2);
+    expect(my_system.customers).toHaveLength(2);
+    expect(spy).toHaveBeenCalledTimes(2);
+  });
+
+  test("Eliminación de clientes", () => {
+    expect(my_system.customers).toHaveLength(2);
+    const spy = vi.spyOn(my_system, "removeCustomer");
+    my_system.removeCustomer(customer1);
+    expect(my_system.customers).toHaveLength(1);
+    expect(spy).toHaveBeenCalledTimes(1);
+    my_system.removeCustomer(customer2);
+    expect(my_system.customers).toHaveLength(0);
+    expect(spy).toHaveBeenCalledTimes(2);
   });
 });
