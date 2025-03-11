bccd6728c87c00668b93a2d667f72a0692c19a2e
Alejandro M. Linares <alu0101476476@ull.edu.es>
2025-03-11 16:52:19 +0000

    comentarios inventory system - configuración coverage

--- src/inventory_system.ts
+++ src/inventory_system.ts
@@ -12,34 +12,68 @@ export class InventorySystem {
   get merchants() { return this._merchants; }; 
   get customers() { return this._customers; }; 
 
+  /**
+   * Añade un item al inventario
+   * @param item - (Good) item que se añadirá
+   */
   addItem(item: Good) {
     this._items.push(item);
   }
 
+  /**
+   * Elimina un item con un id específico
+   * @param id - (number) id del objeto
+   */
   removeItem(id: number) {
     this._items = this._items.filter(item => item.id !== id);
   }
 
-  listItems(orderBy: 'name' | 'value' = 'name', ascending: boolean = true) {
+  /**
+   * Lista todos los items almacenados en orden ascendente por defecto
+   * @param orderBy - (string) ordena los objetos según el atributo seleccionado
+   * @param ascending - (bool?) si queremos orden asscendente o descendente
+   * @returns - (Good[]) lista de bienes que tenemos en el inventario
+   */
+  listItems(orderBy: 'name' | 'value' = 'name', ascending: boolean = true): Good[] {
     return this._items.sort((a, b) => {
       const factor = ascending ? 1 : -1;
       return a[orderBy] > b[orderBy] ? factor : -factor;
     });
   }
 
+  /**
+   * Método que añade un mercader a nuestro sistema
+   * @param merchant - (Merchant) 
+   */
   addMerchant(merchant: Merchant) {
     this._merchants.push(merchant);
   }
 
+  /**
+   * Método que añade un cliente a nuestro sistema
+   * @param customer 
+   */
   addCustomer(customer: Customer) {
     this._customers.push(customer);
   }
 
-  findMerchantByName(name: string) {
+  /**
+   * Método que nos permite encontrar a un mercader 
+   * dentro de nuestro sistema mediante su nombre
+   * @param name - (string) nombre del mercader
+   * @returns - (merchant)
+   */
+  findMerchantByName(name: string): Merchant {
     return this._merchants.find(m => m.name.toLowerCase() === name.toLowerCase());
   }
 
-  findCustomerByName(name: string) {
+  /**
+   * Método que nos permite encontrar a un cliente 
+   * dentro de nuestro sistema mediante su nombre
+   * @param name - (string)
+   * @returns - (Customer)
+   */
+  findCustomerByName(name: string): Customer {
     return this._customers.find(c => c.name.toLowerCase() === name.toLowerCase());
   }
 }
